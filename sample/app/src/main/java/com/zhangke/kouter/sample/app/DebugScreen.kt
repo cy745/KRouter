75b6f83426b75077b1afc8b69c7299de4c4bde0b
lalilu <1248393538@qq.com>
2024-08-10 18:31:24 +0800

    refactor: 调整测试代码

--- sample/app/src/main/java/com/zhangke/kouter/sample/app/DebugScreen.kt
+++ sample/app/src/main/java/com/zhangke/kouter/sample/app/DebugScreen.kt
@@ -3,7 +3,7 @@ package com.zhangke.kouter.sample.app
 import com.zhangke.krouter.annotation.Destination
 import com.zhangke.krouter.sample.core.Screen
 
-@Destination(router = ["screen/test"], remark = "")
+@Destination(router = ["screen/test"])
 class DebugScreen(
     val name: String = "debugScreen",
     val title: String?,
